073f585c727f15dfb40657cd9842870270724029
SHIVAM SINGH <shivam@SHIVAMs-MacBook-Air.local>
2024-10-02 19:18:51 -0400

    people screen added and styled

---
+++ src/Kanbas/Courses/People/Table.tsx
@@ -0,0 +1,85 @@
+import { FaUserCircle } from "react-icons/fa";
+export default function PeopleTable() {
+  return (
+    <div id="wd-people-table">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Login ID</th>
+            <th>Section</th>
+            <th>Role</th>
+            <th>Last Activity</th>
+            <th>Total Activity</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Tony</span>{" "}
+              <span className="wd-last-name">Stark</span>
+            </td>
+            <td className="wd-login-id">001234567S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-01</td>
+            <td className="wd-total-activity">10:21:32</td>{" "}
+          </tr>
+          {/* Add at least 3 more users such as Bruce Wayne, Steve Rogers, and Natasha Romanoff */}
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Steve</span>{" "}
+              <span className="wd-last-name">Rogers</span>
+            </td>
+            <td className="wd-login-id">001234565S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-01</td>
+            <td className="wd-total-activity">09:22:38</td>{" "}
+          </tr>
+
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Bruce</span>{" "}
+              <span className="wd-last-name">Wayne</span>
+            </td>
+            <td className="wd-login-id">001234563S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">1999-01-23</td>
+            <td className="wd-total-activity">10:21:32</td>{" "}
+          </tr>
+
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Natasha</span>{" "}
+              <span className="wd-last-name">Romanoff</span>
+            </td>
+            <td className="wd-login-id">001234561S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">1998-10-01</td>
+            <td className="wd-total-activity">14:10:05</td>{" "}
+          </tr>
+
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Ryan</span>{" "}
+              <span className="wd-last-name">Reynolds</span>
+            </td>
+            <td className="wd-login-id">001234562S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2000-12-12</td>
+            <td className="wd-total-activity">03:21:32</td>{" "}
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
