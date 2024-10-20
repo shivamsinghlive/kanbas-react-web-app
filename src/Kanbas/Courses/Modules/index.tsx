3b0936e061f850d9cb34f0586e90a5b0f299e4d7
SHIVAM SINGH <shivam@SHIVAMs-MacBook-Air.local>
2024-10-20 01:54:27 -0400

    fixing modules

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,17 +1,27 @@
 import ModulesControls from "./ModulesControls";
 import LessonControlButtons from "./LessonControlButtons";
-import { BsGripVertical } from 'react-icons/bs';
+import { BsGripVertical } from "react-icons/bs";
 import ModuleControlButtons from "./ModulesControlButton";
+import * as db from "../../Database";
+import { useParams } from "react-router";
 
 export default function Modules() {
+
+  const { cid } = useParams();
+  const modules = db.modules;
+
   return (
     <div>
-      <ModulesControls /><br /><br /><br /><br />
-      <ul id="wd-modules" className="list-group rounded-0">
+      <ModulesControls />
+      <br />
+      <br />
+      <br />
+      <br />
+      {/* <ul id="wd-modules" className="list-group rounded-0">
         <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
           <div className="wd-title p-3 ps-2 bg-secondary">
             <BsGripVertical className="me-2 fs-3" />
-            Week 
+            Week
             <ModuleControlButtons />
           </div>
           <ul className="wd-lessons list-group rounded-0">
@@ -26,26 +36,64 @@ export default function Modules() {
               <LessonControlButtons />
             </li>
 
-
             <li className="wd-lesson list-group-item p-3 ps-1">
               <BsGripVertical className="me-2 fs-3" />
-              Learn what is Web Development <LessonControlButtons /> </li>
+              Learn what is Web Development <LessonControlButtons />{" "}
+            </li>
             <li className="wd-lesson list-group-item p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" /> LESSON 1 <LessonControlButtons /></li>
-            <li className="wd-lesson list-group-item p-3 ps-1"> <BsGripVertical className="me-2 fs-3" /> LESSON 2 <LessonControlButtons /></li>
+              <BsGripVertical className="me-2 fs-3" /> LESSON 1{" "}
+              <LessonControlButtons />
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              {" "}
+              <BsGripVertical className="me-2 fs-3" /> LESSON 2{" "}
+              <LessonControlButtons />
+            </li>
           </ul>
         </li>
         <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary"> <BsGripVertical className="me-2 fs-3" /> Week 2 <ModuleControlButtons /> </div>
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            {" "}
+            <BsGripVertical className="me-2 fs-3" /> Week 2{" "}
+            <ModuleControlButtons />{" "}
+          </div>
           <ul className="wd-lessons list-group rounded-0">
             <li className="wd-lesson list-group-item p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" />LEARNING OBJECTIVES <LessonControlButtons /></li>
-            <li className="wd-lesson list-group-item p-3 ps-1"> <BsGripVertical className="me-2 fs-3" /> LESSON 1 <LessonControlButtons /> </li>
-            <li className="wd-lesson list-group-item p-3 ps-1"> <BsGripVertical className="me-2 fs-3" /> LESSON 2  <LessonControlButtons /></li>
+              <BsGripVertical className="me-2 fs-3" />
+              LEARNING OBJECTIVES <LessonControlButtons />
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              {" "}
+              <BsGripVertical className="me-2 fs-3" /> LESSON 1{" "}
+              <LessonControlButtons />{" "}
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              {" "}
+              <BsGripVertical className="me-2 fs-3" /> LESSON 2{" "}
+              <LessonControlButtons />
+            </li>
           </ul>
         </li>
-      </ul> </div>
+      </ul>{" "} */}
+      <ul id="wd-modules" className="list-group rounded-0">
+
+{modules
+          .filter((module: any) => module.course === cid)
+          .map((module: any) => (
+          <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+            <div className="wd-title p-3 ps-2 bg-secondary">
+              <BsGripVertical className="me-2 fs-3" /> {module.name} <ModuleControlButtons />
+            </div>
+            {module.lessons && (
+              <ul className="wd-lessons list-group rounded-0">
+                {module.lessons.map((lesson: any) => (
+                  <li className="wd-lesson list-group-item p-3 ps-1">
+                    <BsGripVertical className="me-2 fs-3" /> {lesson.name} <LessonControlButtons />
+                  </li>
+                ))}</ul>)}</li>))}</ul>
 
 
-  );
+    </div>
+              );
+  
 }
