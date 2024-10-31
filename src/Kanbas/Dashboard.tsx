c46648760883b3ccacb80b1549fb6b5497f8a040
SHIVAM SINGH <shivam@SHIVAMs-MacBook-Air.local>
2024-10-31 10:59:22 -0400

    course update 3.1.1 part 1

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -176,11 +176,30 @@
 
 import { Link } from "react-router-dom";
 import * as db from "./Database";
+import { useState } from "react";
 export default function Dashboard() {
-  const courses = db.courses;
+  // const courses = db.courses;
+  const [courses, setCourses] = useState<any[]>(db.courses);
+  const course: any = {
+    _id: "0", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    img: "/images/reactjs.jpg", description: "New Description"
+  };
+  const addNewCourse = () => {
+    const newCourse = { ...course,
+                        _id: new Date().getTime().toString() };
+    setCourses([...courses, { ...course, ...newCourse }]);
+  };
+
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h5>New Course
+          <button className="btn btn-primary float-end"
+                  id="wd-add-new-course-click"
+                  onClick={addNewCourse} > Add </button>
+      </h5><hr />
+
       <h2 id="wd-dashboard-published">
         Published Courses ({courses.length})
       </h2>{" "}
