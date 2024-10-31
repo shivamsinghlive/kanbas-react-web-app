78fdf9a11613fe2b5ee7ab72e8308f52d7d5c27a
SHIVAM SINGH <shivam@SHIVAMs-MacBook-Air.local>
2024-10-31 09:30:48 -0400

    lab4 update

--- src/Labs/Lab4/ReduxExamples/index.tsx
+++ src/Labs/Lab4/ReduxExamples/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HelloRedux from "./HelloRedux";
 import CounterRedux from "./CounterRedux";
+import AddRedux from "./AddRedux";
 
 export default function ReduxExamples() {
   return(
@@ -9,7 +10,10 @@ export default function ReduxExamples() {
       <HelloRedux />
       <hr />
       <CounterRedux/>
+      <hr />
+      <AddRedux />
     </div>
+
   );
 };
 
