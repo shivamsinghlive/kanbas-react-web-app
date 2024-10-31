bf5b35bb2f1ca294538a69ffbbd8b7159adb8135
SHIVAM SINGH <shivam@SHIVAMs-MacBook-Air.local>
2024-10-31 14:53:12 -0400

    module reducer

--- src/Kanbas/Courses/Modules/ModulesControlButton.tsx
+++ src/Kanbas/Courses/Modules/ModulesControlButton.tsx
@@ -3,12 +3,21 @@ import { BsPlus } from "react-icons/bs";
 import GreenCheckmark from "./GreenCheckmark";
 import { FaTrash } from "react-icons/fa6";
 
-export default function ModuleControlButtons(
-  { moduleId, deleteModule }: { moduleId: string; deleteModule: (moduleId: string) => void; } 
-) {
+export default function ModuleControlButtons({
+  moduleId,
+  deleteModule,
+  editModule,
+}: {
+  moduleId: string;
+  deleteModule: (moduleId: string) => void;
+  editModule: (moduleId: string) => void;
+}) {
   return (
     <div className="float-end">
-      <FaTrash className="text-danger me-2 mb-1" onClick={() => deleteModule(moduleId)}/>
+      <FaTrash
+        className="text-danger me-2 mb-1"
+        onClick={() => deleteModule(moduleId)}
+      />
 
       <GreenCheckmark />
       <BsPlus className="fs-4" />
--- src/Kanbas/Courses/Modules/ModulesControls.tsx
+++ src/Kanbas/Courses/Modules/ModulesControls.tsx
@@ -2,11 +2,23 @@ import { FaPlus } from "react-icons/fa6";
 import GreenCheckmark from "./GreenCheckmark";
 import ModuleEditor from "./ModuleEditor";
 
-export default function ModulesControls({ moduleName, setModuleName, addModule }:
-  { moduleName: string; setModuleName: (title: string) => void; addModule: () => void; }) {
+export default function ModulesControls({
+  moduleName,
+  setModuleName,
+  addModule,
+}: {
+  moduleName: string;
+  setModuleName: (title: string) => void;
+  addModule: () => void;
+}) {
   return (
     <div id="wd-modules-controls" className="text-nowrap">
-      <button id="wd-add-module-btn" className="btn btn-lg btn-danger me-1 float-end">
+      <button
+        id="wd-add-module-btn"
+        className="btn btn-lg btn-danger me-1 float-end"
+        data-bs-toggle="modal"
+        data-bs-target="#wd-add-module-dialog"
+      >
         <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
         Module
       </button>
@@ -68,26 +80,25 @@ export default function ModulesControls({ moduleName, setModuleName, addModule }
           id="wd-publish-all-btn"
           className="btn btn-lg btn-secondary "
           type="button"
-          
         >
-          
           View Progress
         </button>
-        </div>
-        <div className=" d-inline me-1 float-end">
+      </div>
+      <div className=" d-inline me-1 float-end">
         <button
           id="wd-publish-all-btn"
           className="btn btn-lg btn-secondary "
           type="button"
-          
         >
-          
           Collapse All
         </button>
-        <ModuleEditor dialogTitle="Add Module" moduleName={moduleName}
-                    setModuleName={setModuleName} addModule={addModule} />
-
-        </div>
+        <ModuleEditor
+          dialogTitle="Add Module"
+          moduleName={moduleName}
+          setModuleName={setModuleName}
+          addModule={addModule}
+        />
+      </div>
     </div>
   );
 }
