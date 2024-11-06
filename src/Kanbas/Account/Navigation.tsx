4007cf406a6e8c03cafc92815f45d47c4de2ca55
SHIVAM SINGH <shivam@SHIVAMs-MacBook-Air.local>
2024-11-05 20:20:47 -0500

    feature 3.4.5 added

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,7 +1,10 @@
 import { link } from "fs";
+import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 
 export default function AccountNavigation() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
   const { pathname } = useLocation();
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -3,7 +3,10 @@ import Signin from "./Signin";
 import { Routes, Route, Navigate } from "react-router";
 import Signup from "./Signup";
 import AccountNavigation from "./Navigation";
+import { useSelector } from "react-redux";
 export default function Account() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+
   return (
     <div id="wd-account-screen">
       <h2>Account</h2>
@@ -14,7 +17,7 @@ export default function Account() {
           </td>
           <td valign="top">
       <Routes>
-        <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
+        <Route path="/" element={<Navigate to={currentUser ? "/Kanbas/Account/Profile" : "/Kanbas/Account/Signin" } />} />
         <Route path="/Signin" element={<Signin />} />
         <Route path="/Profile" element={<Profile />} />
         <Route path="/Signup" element={<Signup />} />
