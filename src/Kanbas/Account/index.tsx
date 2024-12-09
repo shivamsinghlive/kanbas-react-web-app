2eca664e57926375e3e31de04d37128085771e0a
SHIVAM SINGH <shivam@SHIVAMs-MacBook-Air.local>
2024-12-08 19:52:01 -0500

    code update for netlify

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -17,7 +17,7 @@ export default function Account() {
             <td valign="top">
               <Routes>
                 <Route path="/"
-                       element={<Navigate to={ currentUser ? "/Kanbas/Account/Profile" : "Kanbas/Account/Signin" }/>} />
+                       element={<Navigate to={ currentUser ? "/Kanbas/Account/Profile" : "/Kanbas/Account/Signin" }/>} />
                 <Route path="/Signin" element={<Signin />} />
                 <Route path="/Profile" element={<Profile />} />
                 <Route path="/Signup" element={<Signup />} />
